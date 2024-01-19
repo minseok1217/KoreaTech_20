017225e1dba00eca5da42ede5f748933e43e84bc
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-01-19 20:02:46 +0900

    .

---
+++ koreatech20-code/koreatech20/src/test/java/koreatech20/koreatech20/Koreatech20ApplicationTests.java
@@ -0,0 +1,13 @@
+package koreatech20.koreatech20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Koreatech20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
